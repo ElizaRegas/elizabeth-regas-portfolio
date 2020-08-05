a337840e54e03c31e34886f6f7fc7420c21f15b7
Eliza Regas <elizaregas@gmail.com>
2020-08-05 19:37:25 -0400

    paths added

--- src/App.js
+++ src/App.js
@@ -15,16 +15,16 @@ class App extends Component {
         <div className="content">
           <Navbar />
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
             </Route>
-            <Route path="/about">
+            <Route exact path="/about">
               <About />
             </Route>
-            <Route path="/portfolio">
+            <Route exact path="/portfolio">
               <Portfolio />
             </Route>
-            <Route path="/contact">
+            <Route exact path="/contact">
               <Contact />
             </Route>
           </Switch>
