80fc2531499632f7fa59c045586943265d9bb9c4
Eliza Regas <elizaregas@gmail.com>
2020-08-06 17:12:35 -0400

    first portfolio item added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 import "./home.css";
+import "../../App.css";
 
 class Home extends Component {
   render() {
     return (
       <div id="heroImage" className="content">
-        <div uk-scrollspy="cls:uk-animation-fade">
-          <div className="homeText" id="homeName">
-            Elizabeth Regas
-          </div>
+        <div className="homeText" id="homeName">
+          Elizabeth Regas
         </div>
-        <div className="uk-animation-kenburns">
-          <div className="homeText">
-            Developer. Designer. Manipulator of the DOM.
+        <div className="uk-animation-fade">
+          <div className="uk-animation-kenburns">
+            <div className="homeText">
+              Developer. Designer. Manipulator of the DOM.
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,26 +1,94 @@
 import React from "react";
 import ekHome from "../../images/ekHome.png";
-import ekMusic from "../../images/ekMusic.png";
-import ekContact from "../../images/ekContact.png";
 import "./portfolio.css";
+import "../../App.css";
 
 const Portfolio = () => {
   return (
     <div>
-      <div
-        class="uk-grid-small uk-child-width-1-4@s uk-flex-center uk-text-center"
-        uk-grid
-      >
-        <div className="portOne">The Eliza Kelley Project</div>
-        <div>
-          <img src={ekHome} alt="eliza kelley home page" id="ekHome"></img>
-        </div>
-        <div>
-          <img src={ekMusic} alt="eliza kelley music page" id="ekMusic"></img>
+      <div className="uk-grid">
+        <div
+          class="uk-child-width-1-2@s uk-child-width-1-3@m uk-card-body"
+          uk-grid="masonry: true"
+        >
+          <div>
+            <div
+              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
+              //style="height: 100em"
+            >
+              <img
+                src={ekHome}
+                alt="eliza kelley home page"
+                className="ekProject"
+              ></img>
+            </div>
+          </div>
+          <div>
+            <div
+              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
+              //style="height: 130em"
+            >
+              Item
+            </div>
+          </div>
+          <div>
+            <div
+              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
+              //style="height: 150em"
+            >
+              Item
+            </div>
+          </div>
+          <div>
+            <div
+              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
+              //style="height: 160em"
+            >
+              Item
+            </div>
+          </div>
+          <div>
+            <div
+              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
+              //style="height: 120em"
+            >
+              Item
+            </div>
+          </div>
+          <div>
+            <div
+              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
+              //style="height: 140em"
+            >
+              Item
+            </div>
+          </div>
+          <div>
+            <div
+              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
+              //style="height: 200em"
+            >
+              Item
+            </div>
+          </div>
+          <div>
+            <div
+              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
+              //style="height: 180em"
+            >
+              Item
+            </div>
+          </div>
+          <div>
+            <div
+              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
+              //style="height: 140em"
+            >
+              Item
+            </div>
+          </div>
         </div>
       </div>
-
-      <img src={ekContact} alt="eliza kelley contact page" id="ekContact"></img>
     </div>
   );
 };
