ddfbcf6cb535714f2329d59ae37208d45556d307
Eliza Regas <elizaregas@gmail.com>
2020-08-06 19:35:58 -0400

    av portfolio items in place

--- src/pages/AVPortfolio/AVPortfolio.jsx
+++ src/pages/AVPortfolio/AVPortfolio.jsx
@@ -13,42 +13,30 @@ import "../../App.css";
 const AVPortfolio = () => {
   return (
     <div>
+      <div className="portfolio">Audio Video Portfolio</div>
       <div className="uk-grid">
         <div
           class="uk-child-width-1-2@s uk-child-width-1-3@m"
           uk-grid="masonry: true"
         >
           <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://www.youtube.com/watch?v=0zL5rEqbXvE" target="_blank" rel="noopener noreferrer">
-                <img
-                  src={SD}
-                  alt="Sweet Dreams"
-                  className="customBorder"
-                ></img>
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body">
+              <a
+                href="https://www.youtube.com/watch?v=0zL5rEqbXvE"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={SD} alt="Sweet Dreams" className="customBorder"></img>
               </a>
             </div>
           </div>
           <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://elizakelley.com/" target="_blank" rel="noopener noreferrer">
-                <img
-                  src={ekHome}
-                  alt="eliza kelley home page"
-                  className="customBorder"
-                ></img>
-              </a>
-            </div>
-          </div>
-          <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://www.youtube.com/watch?v=1dF_CofdFX4" target="_blank" rel="noopener noreferrer">
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body">
+              <a
+                href="https://www.youtube.com/watch?v=1dF_CofdFX4"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img
                   src={DSB}
                   alt="Don't Stop Believin'"
@@ -58,103 +46,14 @@ const AVPortfolio = () => {
             </div>
           </div>
           <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://www.youtube.com/watch?v=u6m-f9-bmWU" target="_blank" rel="noopener noreferrer">
-                <img
-                  src={UR}
-                  alt="Uprising"
-                  className="customBorder"
-                ></img>
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body">
+              <a
+                href="https://www.youtube.com/watch?v=u6m-f9-bmWU"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={UR} alt="Uprising" className="customBorder"></img>
               </a>
-            </div>
-          </div>
-          <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://catsagainstbananatrees.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-                <img
-                  src={CABT}
-                  alt="cat against a banana tree"
-                  className="customBorder"
-                ></img>
-              </a>
-            </div>
-          </div>
-          <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://www.youtube.com/watch?v=1dF_CofdFX4" target="_blank" rel="noopener noreferrer">
-                <img
-                  src={DSB}
-                  alt="Don't Stop Believin'"
-                  className="customBorder"
-                ></img>
-              </a>
-            </div>
-          </div>
-          <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://elizaregas.github.io/Good-Spirits/" target="_blank" rel="noopener noreferrer">
-                <img
-                  src={GS}
-                  alt="cat against a banana tree"
-                  className="customBorder"
-                ></img>
-              </a>
-            </div>
-          </div>
-          <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
-              //style="height: 160em"
-            >
-              Item
-            </div>
-          </div>
-          <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
-              //style="height: 120em"
-            >
-              Item
-            </div>
-          </div>
-          <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
-              //style="height: 140em"
-            >
-              Item
-            </div>
-          </div>
-          <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
-              //style="height: 200em"
-            >
-              Item
-            </div>
-          </div>
-          <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
-              //style="height: 180em"
-            >
-              Item
-            </div>
-          </div>
-          <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle"
-              //style="height: 140em"
-            >
-              Item
             </div>
           </div>
         </div>
--- src/pages/WPortfolio/WPortfolio.jsx
+++ src/pages/WPortfolio/WPortfolio.jsx
@@ -13,16 +13,19 @@ import "../../App.css";
 const WPortfolio = () => {
   return (
     <div>
+      <div className="portfolio">Web Development Portfolio</div>
       <div className="uk-grid">
         <div
           class="uk-child-width-1-2@s uk-child-width-1-3@m"
           uk-grid="masonry: true"
         >
           <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://catsagainstbananatrees.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body">
+              <a
+                href="https://catsagainstbananatrees.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img
                   src={CABT}
                   alt="cat against a banana tree"
@@ -32,10 +35,12 @@ const WPortfolio = () => {
             </div>
           </div>
           <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://elizakelley.com/" target="_blank" rel="noopener noreferrer">
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body">
+              <a
+                href="https://elizakelley.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img
                   src={ekHome}
                   alt="eliza kelley home page"
@@ -45,23 +50,23 @@ const WPortfolio = () => {
             </div>
           </div>
           <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://elizaregas.github.io/Good-Spirits/" target="_blank" rel="noopener noreferrer">
-                <img
-                  src={GS}
-                  alt="good spirits"
-                  className="customBorder"
-                ></img>
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body">
+              <a
+                href="https://elizaregas.github.io/Good-Spirits/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={GS} alt="good spirits" className="customBorder"></img>
               </a>
             </div>
           </div>
           <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://elizaregas.github.io/hw6-weather-dashboard/" target="_blank" rel="noopener noreferrer">
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body">
+              <a
+                href="https://elizaregas.github.io/hw6-weather-dashboard/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img
                   src={Weather}
                   alt="weather app"
@@ -71,10 +76,12 @@ const WPortfolio = () => {
             </div>
           </div>
           <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://elizaregas.github.io/hw4-javascript-quiz/" target="_blank" rel="noopener noreferrer">
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body">
+              <a
+                href="https://elizaregas.github.io/hw4-javascript-quiz/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img
                   src={JSQuiz}
                   alt="javascript quiz"
@@ -84,10 +91,12 @@ const WPortfolio = () => {
             </div>
           </div>
           <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://elizaregas.github.io/Trivia_YDKJ/" target="_blank" rel="noopener noreferrer">
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body">
+              <a
+                href="https://elizaregas.github.io/Trivia_YDKJ/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img
                   src={YDKJ}
                   alt="javascript quiz"
@@ -97,10 +106,12 @@ const WPortfolio = () => {
             </div>
           </div>
           <div>
-            <div
-              class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"
-            >
-              <a href="https://enthousiaste-maison-76258.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body">
+              <a
+                href="https://enthousiaste-maison-76258.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img
                   src={Burger}
                   alt="burger app"
@@ -108,6 +119,9 @@ const WPortfolio = () => {
                 ></img>
               </a>
             </div>
+          </div>
+          <div>
+            <div class="uk-card uk-card-default uk-flex uk-flex-center uk-flex-middle uk-card-body"></div>
           </div>
         </div>
       </div>
